95225891728e30d8fbd896675c4562427babd4cb
James Tang <james@goodnotesapp.com>
2020-05-26 11:42:55 +0800

    Initial Commit

---
+++ testSegmentFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testSegmentFirebase
+//
+//  Created by James Tang on 26/5/2020.
+//  Copyright © 2020 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
